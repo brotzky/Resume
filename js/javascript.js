236779e55086abdb3d663996cc31c981e53021ca
Dennis Brotzky <brotzky@gmail.com>
2015-02-15 20:45:32 -0500

    updated with new sections

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,16 @@
-// Testing Grunt
+$(function() {
+
+  $(window).scroll(function(){
+    var nav = $('.navigation');
+      if($(window).scrollTop() > 2)
+      {
+        nav.addClass('shadow');
+      }
+      else {
+        nav.removeClass('shadow');
+      }
+   });
+
+})
+
+
